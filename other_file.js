c9889246c059b2246145d8d140806750a265aff9
Imran Iqbal <imran1337@gmail.com>
2019-08-01 00:24:44 +0000

    Automatically backed up by Learn

--- other_file.js
+++ other_file.js
@@ -1,9 +1,3 @@
 var index = require("./index.js")
 
 console.log(index.name)
-
-//if (name === "Susan") {
- // console.log("Was expecting Joe") 
-}//   else {
- //   console.log("That's correct!") 
-// }
